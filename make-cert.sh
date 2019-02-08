a213f357d44d52079342fe0353eae3067676bca5
Christopher Morrison <Chris.Morrison500@gmail.com>
2019-02-07 20:37:30 -0500

    update key sign script

--- make-cert.sh
+++ make-cert.sh
@@ -1,4 +1,9 @@
 mkdir certs
-openssl genrsa -des3 -out certs/myCA.key 3072
-openssl req -x509 -new -nodes -key certs/myCA.key -sha256 -days 1825 -out certs/myCA.pem
+#openssl genrsa -des3 -out certs/myCA.key 3072
+#openssl req -x509 -new -nodes -key certs/myCA.key -sha256 -days 1825 -out certs/myCA.pem
+
+#openssl req -out CSR.csr -new -newkey rsa:2048 -nodes -keyout privateKey.key
+
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout \*.key -out \*.crt
+
 
